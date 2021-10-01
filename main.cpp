be33139c2ad5387b0ad7c7467b15e3cedd0c6248
Thomas Harrison <twh2898@vt.edu>
2021-10-01 16:26:31 -0400

    Working read_csv

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 #include <fmt/core.h>
 
 #include <fstream>
+#include <sstream>
 #include <string>
+#include <string_view>
 
 #include "Shader.hpp"
 #include "context.hpp"
@@ -13,8 +15,59 @@
 static const std::vector<int> one = {0, 1};
 static const std::vector<int> two = {2, 3};
 
-static void write_buffer(const std::string & filename, const Table::Ptr & table) {
-    std::ofstream os(filename);
+static Table::Ptr read_csv(const std::string_view & filename) {
+    std::ifstream is(filename.data());
+    if (!is.is_open())
+        return nullptr;
+
+    std::vector<int> data;
+    std::string line;
+    int val;
+    int width;
+    int height;
+
+    if (is.good()) {
+        std::getline(is, line);
+        std::stringstream ss(line);
+        while (ss >> val) {
+            data.push_back(val);
+            if (ss.peek() == ',')
+                ss.ignore();
+        }
+        width = data.size();
+        height = 1;
+    }
+
+    while (std::getline(is, line)) {
+        std::stringstream ss(line);
+        while (ss >> val) {
+            data.push_back(val);
+            if (ss.peek() == ',')
+                ss.ignore();
+        }
+        height++;
+    }
+
+    is.close();
+
+    std::string_view tableName = filename;
+
+    auto slashPos = tableName.rfind('/');
+    if (slashPos != std::string_view::npos) {
+        tableName.remove_prefix(slashPos + 1);
+    }
+
+    auto dotPos = tableName.find('.');
+    if (dotPos != std::string_view::npos) {
+        tableName.remove_suffix(tableName.size() - dotPos);
+    }
+
+    fmt::print("Table name is {} from {}\n", tableName, filename);
+    return Table::fromTable(tableName, data, width, height);
+}
+
+static void write_csv(const std::string_view & filename, const Table::Ptr & table) {
+    std::ofstream os(filename.data());
 
     for (int r = 0; r < table->getHeight(); r++) {
         for (int c = 0; c < table->getWidth(); c++) {
@@ -35,13 +88,11 @@ static int render_with(const Context & context) {
     if (!shader)
         return 1;
 
-    auto buff1 =
-        Table::fromTable("one", one, context.getWidth(), context.getHeight());
+    auto buff1 = read_csv("one.csv");
     if (!buff1)
         return 2;
 
-    auto buff2 =
-        Table::fromTable("two", two, context.getWidth(), context.getHeight());
+    auto buff2 = read_csv("two.csv");
     if (!buff2)
         return 3;
 
@@ -71,7 +122,7 @@ static int render_with(const Context & context) {
 }
 
 int main() {
-    Context context(2, 1);
+    Context context(90, 90);
     fmt::print("Context created\n");
 
     int res = render_with(context);
@@ -83,7 +134,7 @@ int main() {
     auto output = std::make_shared<Table>("output", context.getWidth(),
                                           context.getHeight());
     output->readFromPixels();
-    write_buffer("output.csv", output);
+    write_csv("output.csv", output);
 
     return 0;
 }
